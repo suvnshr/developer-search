5532edad3ab151411cab4a5ef7bba7689afdf84d
= <36293610+suvansh-rana@users.noreply.github.com>
2020-03-25 19:38:15 +0530

    added tests and removed unnecessary requirements

--- search/tests.py
+++ search/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.conf import settings
 
 # Create your tests here.
+
+
+class ProductionTestCase(TestCase):
+
+    def test_is_debug_off(self):
+        """ ensures that the project has DEBUG set to False """
+
+        self.assertFalse(settings.DEBUG, "DEBUG is ON, not ready for production !")
+
+
+    def test_is_https_forced(self):
+        """ 
+            ensures that the settings which ensure that https is enforced on site, 
+            are enabled and correct. 
+        """
+
+        self.assertTrue(hasattr(settings, 'SECURE_PROXY_SSL_HEADER'),
+                        "SSL header setting not present")
+        self.assertTrue(hasattr(settings, 'SECURE_SSL_REDIRECT'),
+                        "SSL redirect setting not present")
+
+        self.assertEqual(
+            settings.SECURE_PROXY_SSL_HEADER,
+            ('HTTP_X_FORWARDED_PROTO', 'https'), "SSL Header setting wrong"
+        )
+
+        self.assertTrue(settings.SECURE_SSL_REDIRECT,
+                        "SSL redirect setting wrong")
