c516c3d3334d97cd21b593f43b1690b9d6ec4345
= <36293610+suvansh-rana@users.noreply.github.com>
2020-04-29 00:09:54 +0530

    Added credits page

--- search/urls.py
+++ search/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'search'
+
 urlpatterns = [
-    path("", views.index, name="search_api"),
+    path("", views.index, name="search"),
+    path("credits", views.credits, name="credits"),
 ]
