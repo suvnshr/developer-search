b611afe0a6d143242a521894f7c7ac0f2c9add48
= <36293610+suvansh-rana@users.noreply.github.com>
2020-05-02 14:09:30 +0530

    Turned in production

--- developer_search/settings.py
+++ developer_search/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-production = False
+production = True
 
 DEBUG = not production
 ALLOWED_HOSTS = ['devxplore.herokuapp.com'] if production else []
@@ -137,5 +137,5 @@ django_heroku.settings(locals())
 
 # For forcing HTTPS 
 
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
