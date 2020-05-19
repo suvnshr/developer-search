509600e5e8ba03768ea8c47d124a741eb054edf7
Ashish Chawda <56932286+pixan198@users.noreply.github.com>
2020-05-19 16:57:01 +0530

    Add Interactive Sites
    These sites have interactive fun games which on completing each level we learn a new command

--- search/helpers.py
+++ search/helpers.py
@@ -221,6 +221,10 @@ def perform_search(search_query):
             'keywords': ("git", "github", "github link"),
             'domains': ("github.com", )
         },
+        'interactive sites':{
+            'keywords':("practice","interactive"),
+            'domains': ("flexboxfroggy.com","codepip.com/games/")
+        },
     })
 
     return search_data, limit_reached
