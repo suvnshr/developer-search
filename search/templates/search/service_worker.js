dbed55dfa85f3855b3ec167fa94db61b91990d55
Suvansh Rana <36293610+suvansh-rana@users.noreply.github.com>
2020-08-24 23:35:45 +0530

    changed service worker cache name

--- search/templates/search/service_worker.js
+++ search/templates/search/service_worker.js
@@ -1,6 +1,6 @@
 // Base Service Worker implementation.  To use your own Service Worker, set the PWA_SERVICE_WORKER_PATH variable in settings.py
 
-var staticCacheName = "django-pwa-v" + new Date().getTime();
+var staticCacheName = "devxplore_aug_24_2020"
 var filesToCache = [
     './offline/',
 
