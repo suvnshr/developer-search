11057b194c619ce605a69eb9d33c55c3fae7445f
Suvansh Rana <36293610+suvansh-rana@users.noreply.github.com>
2020-08-24 23:16:38 +0530

    Fixed Static url

--- developer_search/settings.py
+++ developer_search/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howtostatic-files/
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
