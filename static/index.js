0b6b977a9744d171ba9bdbd74cd4b8c2b003c641
Suvansh Rana <36293610+suvansh-rana@users.noreply.github.com>
2021-01-09 16:00:53 +0530

    Fixed theme change issue & Tweaked the search tabs scrollbar

--- static/index.js
+++ static/index.js
@@ -2,62 +2,58 @@ const tabs = document.querySelector(".tabs");
 M.Tabs.init(tabs, {});
 
 function prepareForLightTheme(themeToggle) {
-    themeToggle.textContent = "🌞 Day Mode";
-    themeToggle.href = LIGHT_THEME_URL;
+  themeToggle.textContent = "🌞 Day Mode";
+  themeToggle.href = LIGHT_THEME_URL;
+  themeToggle.style.opacity = 1;
 }
 
 function prepareForDarkTheme(themeToggle) {
-    themeToggle.textContent = "🌙 Night Mode";
-    themeToggle.href = DARK_THEME_URL;
+  themeToggle.textContent = "🌙 Night Mode";
+  themeToggle.href = DARK_THEME_URL;
+  themeToggle.style.opacity = 1;
 }
-
 
 function prepareForNextTheme(themeToggle) {
+  const pageStyle = window.getComputedStyle(document.body);
 
-    const pageStyle = window.getComputedStyle(document.body);
-
-    if (pageStyle['backgroundColor'] === "rgb(34, 34, 34)") {
-        prepareForLightTheme(themeToggle);
-    } else {
-        prepareForDarkTheme(themeToggle);
-    }
+  if (pageStyle["backgroundColor"] === "rgb(34, 34, 34)") {
+    prepareForLightTheme(themeToggle);
+  } else {
+    prepareForDarkTheme(themeToggle);
+  }
 }
 
+window.addEventListener("load", function () {
+  const searchField = document.querySelector("#search-field");
+  const footer = document.querySelector("footer");
+  const themeToggle = document.querySelector("#theme-toggle");
+  const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
-document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(() => prepareForNextTheme(themeToggle), 600);
 
-    const searchField = document.querySelector("#search-field");
-    const footer = document.querySelector("footer");
-    const themeToggle = document.querySelector("#theme-toggle");
-    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+  darkModeMediaQuery.addEventListener("change", (e) => {
+    if (!overide_preferred_color_scheme) {
+      const darkModeOn = e.matches;
+      console.log(darkModeOn);
 
-    prepareForNextTheme(themeToggle);
+      darkModeOn
+        ? prepareForLightTheme(themeToggle)
+        : prepareForDarkTheme(themeToggle);
+      console.log(`Dark mode is ${darkModeOn ? "🌒 on" : "☀️ off"}.`);
+    }
+  });
 
-    darkModeMediaQuery.addListener((e) => {
-
-        if (!overide_preferred_color_scheme) {
-            const darkModeOn = e.matches;
-            darkModeOn ? prepareForLightTheme(themeToggle) : prepareForDarkTheme(themeToggle);
-            console.log(`Dark mode is ${darkModeOn ? '🌒 on' : '☀️ off'}.`);
-        }
-
+  function setListenerToHideFooter() {
+    searchField.addEventListener("focus", function () {
+      if (screen.width < 600) {
+        footer.style.opacity = 0;
+      }
     });
 
-    function setListenerToHideFooter() {
+    searchField.addEventListener("blur", function () {
+      footer.style.opacity = 1;
+    });
+  }
 
-        searchField.addEventListener("focus", function () {
-            if (screen.width < 600) {
-                footer.style.opacity = 0;
-            }
-        });
-
-        searchField.addEventListener("blur", function () {
-            footer.style.opacity = 1;
-        });
-    }
-
-    setListenerToHideFooter();
-
+  setListenerToHideFooter();
 });
-
-
